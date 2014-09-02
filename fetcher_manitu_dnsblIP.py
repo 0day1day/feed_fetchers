8d83a8926816a0065f0747d50c6e7b8311dc14e3
Pierre Lamy <pierre@userid.org>
2014-09-02 10:14:36 -0400

    Add Manitu spam sources

---
+++ fetcher_manitu_dnsblIP.py
@@ -0,0 +1,46 @@
+# Note: the path we are looking for isn't valid every day
+# the file is regen once a week. We drop out if it doesn't exist.
+
+import re
+import shutil
+import requests
+import gzip
+import os
+from datetime import date
+
+# Set the URL path
+url = 'http://www.dnsbl.manitu.net/download/nixspam-ip.dump.gz'
+
+filename = 'nixspam-ip.dump.gz'
+feedID = 'manitu_dnsbl'
+killchain = 'Delivery'
+
+# Check and see if the remote path exists or not
+r = requests.head(url)
+if r.status_code != requests.codes.ok:
+	exit()
+del r
+
+# Fetch the file
+if os.path.isfile(filename): os.remove(filename)
+response = requests.get(url)
+with open(filename, 'wb') as out_file:
+    shutil.copyfileobj(response.raw, out_file)
+del response
+
+# Read the contents
+f = gzip.open(filename, 'rb')
+file_content = f.read()
+f.close()
+splitlines = file_content.split('\n')
+del file_content
+
+print('ipv4,feedID,killchain,description')
+
+for x in splitlines:
+	x = x.strip()
+	if len(x) == 0: continue
+	xlist = x.split()
+	print("%s,%s,%s,%s" % (xlist[1], feedID, killchain, 'Manitu Spam source'))
+	
+if os.path.isfile(filename): os.remove(filename)
